4b170ec6a9b50426113d101fcc0a8132a824397e
Dmitry Malofeev <dim2k2006@yandex.ru>
2021-05-31 18:59:57 +0200

    Update tests

--- __tests__/index.jsx
+++ __tests__/index.jsx
@@ -2,7 +2,22 @@ import React from 'react';
 import Application from '@hexlet/react-todo-app-with-backend';
 import { v4 as uuidV4 } from 'uuid';
 import faker from 'faker';
-import { render } from '../setupTests.js';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import userEvent from '@testing-library/user-event';
+import { render, waitFor } from '../setupTests.js';
+
+const baseUrl = '/api/v1';
+
+const routes = {
+  lists: () => [baseUrl, 'lists'].join('/'),
+  list: (id) => [baseUrl, 'lists', id].join('/'),
+  listTasks: (listId) => [baseUrl, 'lists', listId, 'tasks'].join('/'),
+  listTask: (listId, taskId) => [baseUrl, 'lists', listId, 'tasks', taskId].join('/'),
+  task: (taskId) => [baseUrl, 'tasks', taskId].join('/'),
+};
+
+const server = setupServer();
 
 const buildList = (props = {}) => {
   const {
@@ -51,9 +66,74 @@ const buildPreloadedState = (props = {}) => {
   return preloadedState;
 };
 
+beforeAll(() => server.listen());
+
+afterEach(() => server.resetHandlers());
+
+afterAll(() => server.close());
+
 test('Shows the application.', async () => {
   const preloadedState = buildPreloadedState();
   const { getByText } = render(<Application { ...preloadedState } />);
 
   expect(getByText('Hexlet Todos')).toBeVisible();
+});
+
+test('Creates a task.', async () => {
+  const listId = '1';
+  const list = buildList({ id: listId, name: 'primary', removable: false });
+  const preloadedState = buildPreloadedState({ lists: [list] });
+  const taskText = faker.lorem.word();
+
+  server.use(
+    rest.post('/api/v1/lists/:listId/tasks', (req, res, ctx) => {
+      const task = buildTask({ text: req.body.text, listId: Number(req.params.listId) });
+
+      return res(
+        ctx.json(task),
+      );
+    }),
+  );
+
+  const { getByPlaceholderText, getByText } = render(<Application { ...preloadedState } />);
+
+  userEvent.type(getByPlaceholderText('Please type text...'), taskText);
+
+  userEvent.click(getByText('Add'));
+
+  await waitFor(() => {
+    expect(getByText(taskText)).toBeVisible();
+  });
+});
+
+test('Updates a task.', async () => {
+  const list = buildList({ name: 'primary', removable: false });
+  const task = buildTask({ listId: list.id });
+  const preloadedState = buildPreloadedState({
+    currentListId: list.id,
+    lists: [list],
+    tasks: [task],
+  });
+
+  server.use(
+    rest.patch('/api/v1/tasks/:taskId', (req, res, ctx) => {
+      const { completed } = req.body;
+      const newTask = { ...task, completed, touched: Date.now() };
+
+      return res(
+        ctx.json(newTask),
+      );
+    }),
+  );
+
+  const { getByLabelText, getByRole } = render(<Application { ...preloadedState } />);
+
+  const taskLabel = getByLabelText(task.text);
+  const checkbox = getByRole('checkbox', { name: new RegExp(task.text) });
+
+  userEvent.click(taskLabel);
+
+  await waitFor(() => {
+    expect(checkbox).toBeChecked();
+  });
 });
--- setupTests.js
+++ setupTests.js
@@ -2,4 +2,3 @@ import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
 
 export * from '@testing-library/react';
-export * from '@testing-library/user-event';
