eff97b162eaa31073b34161e0fe31f48a596fa9d
Dmitry Malofeev <dim2k2006@yandex.ru>
2021-05-31 17:38:23 +0200

    Update tests

--- __tests__/index.jsx
+++ __tests__/index.jsx
@@ -1,17 +1,58 @@
 import React from 'react';
 import Application from '@hexlet/react-todo-app-with-backend';
+import { v4 as uuidV4 } from 'uuid';
+import faker from 'faker';
 import { render } from '../setupTests.js';
 
-const preloadedState = {
-  currentListId: 1,
-  lists: [
-    { id: 1, name: 'primary', removable: false },
-    { id: 2, name: 'secondary', removable: true },
-  ],
-  tasks: [],
+const buildList = (props = {}) => {
+  const {
+    id = uuidV4(),
+    name = faker.lorem.word(),
+    removable = true,
+  } = props;
+
+  const list = { id, name, removable };
+
+  return list;
+};
+
+const buildTask = (props = {}) => {
+  const {
+    id = uuidV4(),
+    listId = null,
+    text = faker.lorem.word(),
+    completed = false,
+    touched = Date.now(),
+  } = props;
+
+  const task = {
+    id,
+    listId,
+    text,
+    completed,
+    touched,
+  };
+
+  return task;
+};
+
+const buildPreloadedState = (props = {}) => {
+  const {
+    currentListId = 1,
+    lists = [
+      buildList({ name: 'primary', removable: false }),
+      buildList({ name: 'secondary' }),
+    ],
+    tasks = [],
+  } = props;
+
+  const preloadedState = { currentListId, lists, tasks };
+
+  return preloadedState;
 };
 
 test('Shows the application.', async () => {
+  const preloadedState = buildPreloadedState();
   const { getByText } = render(<Application { ...preloadedState } />);
 
   expect(getByText('Hexlet Todos')).toBeVisible();
